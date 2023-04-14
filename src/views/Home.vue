7236fe8da97e1afaa187e7b038d8a7aa41c879af
Tvqk98 <127388255+Tvqk98@users.noreply.github.com>
2023-04-14 15:10:49 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,9 +11,6 @@
           </div>
           <div class="col-md-6">
             <h3>Tìm hiểu thêm về chúng tôi</h3>
-            <p>
-              Anh ta có nên tìm kiếm niềm vui một cách chính đáng? Không có gì sẽ chỉ trích những niềm vui lớn của cuộc sống của sự thật, bằng cách từ chối nó từ? Tôi tự do, như thể được sinh ra, để tận hưởng những thú vui
-            </p>
              <button class="btn custom-btn btn-outline-primary mr-4">Browse Products</button>
           </div>
         </div>
